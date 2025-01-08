e84e4999a833793f646985f9549d8e81a471f7a7
tonamson <tonamson2@gmail.com>
2025-01-08 11:27:10 +0700

    test wallet connect

--- interface/components/ConnectWalletErc20.tsx
+++ interface/components/ConnectWalletErc20.tsx
@@ -1,8 +1,10 @@
 'use client'
 
 import {createAppKit, useAppKit} from '@reown/appkit/react'
-import { EthersAdapter } from '@reown/appkit-adapter-ethers'
-import { mainnet, arbitrum } from '@reown/appkit/networks'
+import {EthersAdapter} from '@reown/appkit-adapter-ethers'
+import {mainnet, arbitrum} from '@reown/appkit/networks'
+import {useState} from "react";
+import { useDisconnect } from '@reown/appkit/react'
 
 // 1. Get projectId at https://cloud.reown.com
 const projectId = '2af411118080f5c031b70b8d83334bd6'
@@ -16,7 +18,7 @@ const metadata = {
 }
 
 // 3. Create the AppKit instance
-createAppKit({
+const appKit = createAppKit({
 	adapters: [new EthersAdapter()],
 	metadata,
 	networks: [mainnet, arbitrum],
@@ -34,11 +36,25 @@ createAppKit({
 })
 
 export default function ConnectWalletErc20() {
-	const { open } = useAppKit()
-
+	const {open} = useAppKit()
+	const { disconnect } = useDisconnect()
+	const [wallet, setWallet] = useState('Connect')
+	const [isConnect, setIsConnect] = useState(false)
+	
+	appKit.subscribeAccount((account) => {
+		if (account.isConnected && account.status === 'connected') {
+			setWallet(String(account.address))
+			setIsConnect(true)
+		} else {
+			setWallet('Connect')
+			setIsConnect(false)
+		}
+	})
+	
 	return (
 		<>
-			<button className="btn btn-primary" onClick={() => open()}>Connect</button>
+			<button className="btn btn-primary" onClick={() => open()}>{wallet}</button>
+			{isConnect ? <button className="btn btn-primary ms-2" onClick={async () => await disconnect()}>Disconnect</button> : <></>}
 		</>
 	);
 }
